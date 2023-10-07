c2c7502aee181447c438ca3d033ec83fd1bf4e1d
x-maker-cpu <davidmanzi110@gmail.com>
2023-10-07 15:34:56 +0300

    added files to stage

---
+++ index.js
@@ -0,0 +1,18 @@
+function signInHandler(event) {
+    event.preventDefault();
+    window.location.href="./signin.html"
+}
+function leaderBoardHandler(event) {
+    event.preventDefault();
+    window.location.href="./leaderboard.html"
+}
+
+function loginHandler(event) {
+    event.preventDefault();
+    window.location.href="./login.html"
+}
+
+function aboutHandler(event) {
+    event.preventDefault();
+    window.location.href="./about.html"
+}
---
+++ sign.js
@@ -0,0 +1,38 @@
+let latter;
+let age;
+let fullnumber;
+let fullemail;
+let gender;
+
+function latterChangeHandler(event) {
+    console.log("event is :>",event.target.value);
+}
+
+function fullemailChangeHandler(event) {
+    console.log("event is :>",event.target.value)
+}
+
+function fullnumberChangeHandler(event) {
+    console.log("event is :>",event.target.value)
+}
+
+function genderChangeHandler(event) {
+    console.log("event is :>",event.target.value)
+}
+
+function agechangehandler(event) {
+    console.log("event is :>",event.target.value)
+}
+
+function submitchangehandler(event) {
+    event.preventDefault
+    console.log(event.target)  
+}
+
+
+
+
+
+
+
+
